79225626fb6c181feb5d5bbae03ef344307c94fe
Gargi2527 <gargipawar2022.it@mmcoe.edu.in>
2025-02-24 23:09:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const port = 5000;
+
+// Serve static files (CSS, JS, HTML)
+app.use(express.static(path.join(__dirname)));
+
+// Serve the main HTML file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// WebSocket connection handling
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('send name', (user) => {
+        io.emit('send name', user);
+    });
+
+    socket.on('send message', (chat) => {
+        io.emit('send message', chat);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+// Start the server
+server.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
